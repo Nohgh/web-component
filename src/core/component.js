a8a5dd3f1ea3e334791b2fb95a7b832715a04c4e
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-06-11 17:41:55 +0900

    Fix: component core 변경 (props와 mounted를 추가)

--- src/core/component.js
+++ src/core/component.js
@@ -1,23 +1,31 @@
 export default class Component {
   $target;
+  props;
   state;
 
   constructor($target) {
     this.$target = $target;
+    this.props = props;
     this.setup();
+    this.setEvent();
     this.render();
   }
 
   setup() {}
+  mounted() {}
   template() {
     return "";
   }
   render() {
     this.$target.innerHTML = this.template();
+    this.mounted(); //render이후 mounted 실행
   }
   setEvent() {}
   setState(newState) {
     this.state = { ...this.state, ...newState };
     this.render();
+  }
+  addEvent(eventType, selector, callback) {
+    //생략
   }
 }
